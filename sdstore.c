f25bd70a34d3de227802c7f19f1c6807ccf3cacd
DCasalNovo <52653136+DCasalNovo@users.noreply.github.com>
2022-05-27 18:10:51 +0100

    Update sdstore.c

--- sdstore.c
+++ sdstore.c
@@ -10,21 +10,19 @@
 
 #define SIZEOFBUFF 100
 
-void sendArgumento(int fdFifoMain,int argc, char** argv){
+void sendArgumento(int fdFifoMain,int argc, char** argv, char* fifoClient){
     int totalSize = 0;
     char strPid[20];
     char strArgc[20];
 
 
-    char* concateneted = malloc(sizeof(char)*100);
-    int pid = getpid();
+    char* concateneted = malloc(sizeof(char)*150);
 
-    sprintf(strPid, "%d", pid);                                  // pid como string
     sprintf(strArgc,"%d",argc-3);
 
-    strcat(concateneted,strPid);                                 // adiciona pid para enviar
+    strcat(concateneted,fifoClient);                              // adiciona nome do fifo no qual vai receber respostas para enviar
     strcat(concateneted," ");
-    totalSize += strlen(strPid) + 1;
+    totalSize += strlen(fifoClient) + 1;
 
     strcat(concateneted,strArgc);                               // adiciona nr de argumentos para enviar
     strcat(concateneted," ");
@@ -38,16 +36,32 @@ void sendArgumento(int fdFifoMain,int argc, char** argv){
     }
 
     write(fdFifoMain,concateneted,totalSize);                   // envia string concatenada para fifo
-
 }
+
+
+void receiveAnswers(fifoClient){
+    int fdFifo = open(fifoClient, O_RDONLY);
+    int fdFifoWR = open(fifoClient, O_WRONLY);
+
+    while((readBytes = read(fdFifo,buff,SIZEOFBUFF)) > 0){
+        if(readBytes > 0){
+            
+        }
+    }
+}
+
 
 int main(int argc, char** argv){
 
     int fdFifoMain = open("FifoMain",O_WRONLY);
     char buff[SIZEOFBUFF];
     int readBytes;
+    char fifoClient[20];
 
-    sendArgumento(fdFifoMain,argc,argv);
+    sprintf(fifoClient, "%d", pid);
+    mkfifo(fifoClient,0666);
 
+    sendArgumento(fdFifoMain, argc, argv, fifoClient);
 
+    receiveAnswers(fifoClient);
 }
